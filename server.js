af1e34c7a04c0c5330b97af6e6d6aaa178a4709f
virendra038 <virendrapratap038@gmail.com>
2016-05-07 17:57:21 +0530

    Organizing ejs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var morgan = require('morgan')
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+
 var User = require('./models/user');
 
 var app = express();
@@ -17,6 +20,8 @@ mongoose.connect('mongodb://root:abcd123@ds049925.mlab.com:49925/ecommerce',func
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.engine('ejs',engine);
+app.set('view engine','ejs');
 
 
 app.post('/create-user',function(req,res,next){
@@ -27,11 +32,19 @@ app.post('/create-user',function(req,res,next){
 	user.email = req.body.email;
 
 	user.save(function(err){
-		if (err) next(err);
+		if (err) return next(err);
 
 		res.json('Successfully created a new user');
 	});
 
+});
+
+app.get('/',function(req,res){
+	res.render('main/home');
+});
+
+app.get('/about',function(req,res){
+	res.render('main/about');
 });
 
 
